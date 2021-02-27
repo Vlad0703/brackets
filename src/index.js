ad82513ac89d2194b61e0e8416ed960c4ce3236a
Vlad0703 <73859766+Vlad0703@users.noreply.github.com>
2021-02-27 15:28:01 +0300

    Update index.js
    init: start brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let same;
+  for (let el in arr) {
+    if (arr[el][0] === arr[el][1]) {
+      same = arr[el][0]
+    }
+  }
+  let n = 1;
+  let brackets = arr.flat().join('');
+  let stack = [];
+  for (let bracket of input) {
+    let bracketIndex = brackets.indexOf(bracket);
+    if (bracketIndex % 2 === 0) {
+      if (bracket === same && n === 1){
+        stack.push(bracketIndex + 1);
+        n = n*(-1);
+        
+      } else if ((bracket === same && n === -1)){
+        stack.pop();
+        n*= -1;
+        
+      } else {
+        stack.push(bracketIndex + 1);
+      }
+    } else {
+      if (stack.pop() !== bracketIndex) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+
 }
